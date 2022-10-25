dae7fe877343264fb89d5bc30f860568ba09f576
yeligoth <yeligoth@gmail.com>
2022-10-25 10:53:34 -0500

    Initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: 'Jhon',
+    lastName: 'Smith',
+    age: 21
+  },
+  {
+    id: 2,
+    name: 'Jhon 2',
+    lastName: 'Smith 2',
+    age: 22
+  },
+  {
+    id: 3,
+    name: 'Jhon 3',
+    lastName: 'Smith 3',
+    age: 23
+  },
+  {
+    id: 4,
+    name: 'Jhon 4',
+    lastName: 'Smith 4',
+    age: 24
+  },
+  {
+    id: 5,
+    name: 'Jhon 5',
+    lastName: 'Smith 5',
+    age: 25
+  }
+];
